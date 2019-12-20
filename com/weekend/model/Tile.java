2ece6bc25d4c26fba67d4ef98e9d5010244b8e2a
Sarah Chua <43328484+ChuaKaixin@users.noreply.github.com>
2019-12-20 11:38:42 +0800

    Tile - store information of each board tile info

---
+++ com/weekend/model/Tile.java
@@ -0,0 +1,18 @@
+package com.weekend.model;
+
+public class Tile {
+	private String player;
+	private int strength;
+	public String getPlayer() {
+		return player;
+	}
+	public void setPlayer(String player) {
+		this.player = player;
+	}
+	public int getStrength() {
+		return strength;
+	}
+	public void setStrength(int strength) {
+		this.strength = strength;
+	}
+}
